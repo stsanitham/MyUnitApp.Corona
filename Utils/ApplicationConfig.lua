1481eea493e2d47600a4f25eb8e3eefda8b8f563
stsmalarkodi <git+malarkodi@spanenterprises.com>
2016-01-28 10:00:36 +0530

    test changes

--- Utils/ApplicationConfig.lua
+++ Utils/ApplicationConfig.lua
@@ -1,9 +1,13 @@
 ApplicationConfig = {}
 
 
+
+---Malar working----
+
+
 ----------Dev---------------
 
-----[[
+--[[
 IsLive = false
 ApplicationConfig.BASE_URL = "http://api.myunitapp.dotnetethic.com"
 ApplicationConfig.IMAGE_BASE_URL = "http://c.dotnetethic.com/"
@@ -19,7 +23,7 @@ ApplicationConfig.Analysic_TrackId = "UA-51545075-5"
 
 
 -----Stagging--------
---[[
+----[[
 
 IsLive = false
 ApplicationConfig.BASE_URL = "http://api.myunitbuzz.spanunit.com"
